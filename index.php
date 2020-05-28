847bb8f7d749b41ee15b7a8676312d4402c221fc
demzy94 <66075970+demzy94@users.noreply.github.com>
2020-05-28 16:05:25 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>E-Learning Website</title>
 </head>
 <body>
-    
+    <h2>Welcome To E-Learn Website</h2>
 </body>
 </html>
