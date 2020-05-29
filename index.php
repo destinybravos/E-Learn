05467ffdc76b61801d29c8a55a26917e71a23144
demzy94 <udemejohn26@gmail.com>
2020-05-29 14:07:03 +0100

    update

--- index.php
+++ index.php
@@ -1,11 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>E-Learning Website</title>
 </head>
+
 <body>
     <h2>Welcome To E-Learn Website</h2>
+
 </body>
+
 </html>
