fd0a4f3c33162f4c2cda6a9442d591e94c01dbb6
demzy94 <udemejohn26@gmail.com>
2020-06-11 21:40:48 +0100

    img

--- banner.php
+++ banner.php
@@ -1,3 +1,3 @@
-<div style="background-image:url('img/bg2.png'); min-height:70vh; width:100%; background-size:cover; background-position:center;">
+<div style="background-image:url('img/bg2.png'); min-height:74vh; width:100%; background-size:cover; background-position:center;">
 
 </div>
