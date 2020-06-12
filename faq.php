b4a7d54adf27cf5baeb2f82d40e4fa4fa5ef16f9
peterchigozirim <peterchigozirim@gmail.com>
2020-06-12 10:28:55 +0100

    faq

--- faq.php
+++ faq.php
@@ -17,9 +17,68 @@
 <body>
     <?php
     include_once 'head.php';
+    ?>
+ <div class="container" style="margin: 30px auto;">
+        <div class="row">
+            <div class="col-md-8">
+
+                <div id="accordion" class="mb-3">
+                    <div class="card">
+                        <div class="card-header">
+                        <a href="#first-card" class="card-link" data-toggle="collapse">How to create an account </a>
+                        </div>
+                        <div id="first-card" class="collapse show" data-parent="#accordion">
+                         <div class="card-body">
+                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias nihil at fugiat! 
+                         Harum eos voluptatibus adipisci sit aliquid ducimus assumenda rem, quis maiores!
+                          Dolores magnam nihil quisquam deserunt, quod tempore.
+                         </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div id="accordion" class="mb-3">
+                    <div class="card">
+                        <div class="card-header">
+                        <a href="#second-card" class="card-link " data-toggle="collapse">How to join class </a>
+                        </div>
+                        <div id="second-card" class="collapse show" data-parent="#accordion">
+                         <div class="card-body">
+                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias nihil at fugiat! 
+                         Harum eos voluptatibus adipisci sit aliquid ducimus assumenda rem, quis maiores!
+                          Dolores magnam nihil quisquam deserunt, quod tempore.
+                         </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div id="accordion" class="mb-5">
+                    <div class="card">
+                        <div class="card-header">
+                        <a href="#three-card" class="card-link" data-toggle="collapse">How to verify my account </a>
+                        </div>
+                        <div id="three-card" class="collapse show" data-parent="#accordion">
+                         <div class="card-body">
+                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias nihil at fugiat! 
+                         Harum eos voluptatibus adipisci sit aliquid ducimus assumenda rem, quis maiores!
+                          Dolores magnam nihil quisquam deserunt, quod tempore.
+                         </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+
+            <div class="col-md-4">
+                <?php include_once "sidebar.php";  ?>
+            </div>
+        </div>
+    </div>
+    <?php
+    
     include_once 'footer.php';
     ?>
-
 
 </body>
 
